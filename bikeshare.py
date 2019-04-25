2cd6fb31b70876ce66192ae8bbabc0331b284d20
John Fera <johncffera@gmail.com>
2019-04-25 21:55:07 +0200

    Add files via upload

---
+++ bikeshare.py
@@ -0,0 +1,12 @@
+## name: bikeshare.py
+## last modify 19/94/15
+
+## necessary lubarys
+import time
+import pandas as ps
+import numpy as ny
+
+## defination of data file names
+chicago = 'chicago.csv'
+new_york_city = 'new_york_city.csv'
+washington = 'washington.csv'
